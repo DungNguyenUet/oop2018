cb65e4323b735b74a102ff6a6b994d26aeab2ac7
DungNguyenUet <dungnguyenfeb17@gmail.com>
2018-09-20 10:35:38 +0700

    bai nop 8 diem, em chua lam dc cau 12 13

--- src/week1/Student.java
+++ src/week1/Student.java
@@ -9,30 +9,85 @@ public class Student {
     /**
      * Constructor 1
      */
+    private String name;
+    private String id;
+    private String group;
+    private String email;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String x) {
+        name = x;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String x) {
+        id = x;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public void setGroup(String x) {
+        group = x;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String x) {
+        email = x;
+    }
+    //cau 7a
     Student() {
         // TODO:
+        name = "Student";
+        id="000";
+        group="INT22041";
+        email="uet@vnu.edu.vn";
     }
 
     /**
      * Constructor 2
+     *
      * @param n
      * @param sid
      * @param em
      */
     Student(String n, String sid, String em) {
         // TODO:
+        name =n;
+        id=sid;
+        email=em;
+        group="INT22041";
     }
 
     /**
      * Constructor 3
+     *
      * @param s
      */
     Student(Student s) {
         // TODO:
+        name=s.name;
+        id=s.id;
+        group=s.group;
+        email=s.email;
     }
 
-    String getInfo() {
+    void getInfo() {
         // TODO:
-        return null; // xóa dòng này sau khi cài đặt
+        System.out.println("Thong tin sinh vien: "+"\n"+
+                           "Ten Sinh Vien: "+name+"\n"
+                            +"MSSV: "+id+"\n"
+                            +"Group: "+group+"\n"
+                            +"Email: "+email+"\n");
     }
 }
--- src/week1/StudentManagement.java
+++ src/week1/StudentManagement.java
@@ -4,13 +4,29 @@ public class StudentManagement {
 
     // TODO: khai báo thuộc tính students là array chứa các đối tượng thuộc lớp Student (max. 100)
 
-    public boolean sameGroup(Student s1, Student s2) {
+    public static boolean sameGroup(Student s1, Student s2) {
         // TODO:
-        return false; // xóa dòng này sau khi cài đặt
+        if ((s1.getGroup()).equals(s2.getGroup())) return true;
+        else return false; // xóa dòng này sau khi cài đặt
+    }
+    public static void Sw(Student A, Student B){
+        String temp;
+        temp = A.getGroup(); A.setGroup(B.getGroup()); B.setGroup(temp);
+        temp = A.getId(); A.setId(B.getId()); B.setId(temp);
+        temp = A.getName(); A.setName(B.getName()); B.setName(temp);
+        temp = A.getEmail(); A.setEmail(B.getEmail()); B.setEmail(temp);
     }
 
-    void studentsByGroup() {
+
+    static void studentsByGroup(Student[] SVarray) {
         // TODO:
+
+        for(int i=0;i<SVarray.length;i++){
+            if(i>0 && SVarray[i].getGroup()!=SVarray[i-1].getGroup())
+                System.out.println("Danh sach hoc sinh lop "+SVarray[i].getGroup()+" la:");
+            SVarray[i].getInfo();
+
+        }
     }
 
     void removeStudent(String id) {
@@ -19,7 +35,54 @@ public class StudentManagement {
 
     public static void main(String[] args) {
         // TODO:
-        System.out.println("aaaaaaaaaa");
-        System.out.println("aaaaaa");
+        Student S1 = new Student();
+        // todo cau 6:
+        S1.setName("Nguyen Tien Dung");
+        S1.setId("16020908");
+        S1.setGroup("K61CB");
+        S1.setEmail("16020908@vnu.edu.vn");
+        S1.getInfo();
+
+
+        //todo cau 8:
+        Student S2 = new Student();
+        Student S3 = new Student("SVname","SVid","SVem");
+        Student S4 = new Student(S3);
+        Student S5 = new Student(S3);
+        S2.getInfo();
+        S3.getInfo();
+        S4.getInfo();
+        S5.setGroup("INT22042");
+
+
+        //todo test cau 9:
+        StudentManagement SM = new StudentManagement();
+        if(sameGroup(S3,S4)) System.out.println("S3 va S4 cung hoc lop: "+S3.getGroup()+"\n");
+        else System.out.println("S3 va S4 khong hoc cung lop"+"\n");
+        if(sameGroup(S5,S3)) System.out.println("S5 va S3 cung hoc lop: "+S1.getGroup()+"\n");
+        else System.out.println("S5 va S3 khong hoc cung lop"+"\n");
+
+        //todo cau 10:
+        Student[] SVarray = new Student[100];
+        String SVgroup = "lop1";
+        for(int i=0;i<99;i++)
+            SVarray[i].setGroup(SVgroup);
+
+        //todo cau 11:
+        SVarray[99] = new Student();
+        System.out.println("chuan bi in ra");
+        SVarray[99].getInfo();
+        System.out.println(" da in ra");
+        //todo cau 12
+        for(int i=0;i<98;i++)
+            for(int j=i+1;j<99;j++){
+                if(SVarray[i].getGroup().compareTo(SVarray[j].getGroup())==-1)
+                    Sw(SVarray[i],SVarray[j]);
+            }
+        studentsByGroup(SVarray);
+
+
+
+
     }
 }
